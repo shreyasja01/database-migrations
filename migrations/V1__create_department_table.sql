f601224422596a09b73c38bd09cecba0240c02ae
Shreyas Joshi <shreyas.joshi@dev.itcube.net>
2026-07-03 15:14:49 +0530

    Remove migrations folder

--- migrations/V1__create_department_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE testdepartment (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    department_code VARCHAR(20) UNIQUE,
-    department_name VARCHAR(100)
-);
--- migrations/V3__alter_table.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE testemployee ADD COLUMN phone VARCHAR(15);
-ALTER TABLE testemployee ADD COLUMN address VARCHAR(200);
